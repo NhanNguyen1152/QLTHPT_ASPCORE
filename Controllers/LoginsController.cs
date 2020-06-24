673ee4d4afb22471076d431c85076d490601812e
46920791 <46920791+NhanNguyen1152@users.noreply.github.com>
2020-06-24 22:55:18 +0700

    Login

---
+++ Controllers/LoginsController.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QLTHPT.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace QLTHPT.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private readonly acomptec_qlthptContext _context = new acomptec_qlthptContext();
+
+
+        private bool IsAuthenticated(string UserName, string PassWord)
+        {
+            return (UserName == "nhan9961" && PassWord == "12345");
+        }
+
+        // GET: Logins
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Logins/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var login = await _context.Login
+                .FirstOrDefaultAsync(m => m.UserName == id);
+            if (login == null)
+            {
+                return NotFound();
+            }
+
+            return View(login);
+        }
+
+        // GET: Logins/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserName,PassWord")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(login);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(login);
+        }
+
+        // GET: Logins/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var login = await _context.Login.FindAsync(id);
+            if (login == null)
+            {
+                return NotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Logins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([Bind("UserName,PassWord")] Login login)
+        {
+            var chucvu = await _context.Chucvu.FirstOrDefaultAsync(m => m.CvTen == login.UserName && m.CvMa == login.PassWord);
+            
+            if(chucvu != null){
+                HttpContext.Session.SetString("User", login.UserName);
+                
+                return RedirectToAction("Index", "Home", new { area = "Admin"});
+            }
+            else{
+                 return RedirectToAction("Index", "Logins", new { area = "Admin1"});
+            }
+        }
+
+        // GET: Logins/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var login = await _context.Login
+                .FirstOrDefaultAsync(m => m.UserName == id);
+            if (login == null)
+            {
+                return NotFound();
+            }
+
+            return View(login);
+        }
+
+        // POST: Logins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var login = await _context.Login.FindAsync(id);
+            _context.Login.Remove(login);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoginExists(string id)
+        {
+            return _context.Login.Any(e => e.UserName == id);
+        }
+
+
+        //  private bool IsAuthenticated(string UserName, string PassWord)
+        // {
+            // return (UserName == "nhan9961" && PassWord == "12345");
+        // }
+    }
+}
